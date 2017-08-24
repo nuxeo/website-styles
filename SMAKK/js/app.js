74e4c704832654344d5f8c25f230c280e6d69a28
namurray <namurray85@gmail.com>
2017-08-24 11:54:09 -0400

    New Menu

--- SMAKK/js/app.js
+++ SMAKK/js/app.js
@@ -1,1 +1,24 @@
-$(document).foundation()
+$(document).foundation();
+
+//INCLUDES
+
+
+
+//COLLAPSING CARDS
+$(function() {
+    $(".c-collapsing").click(function (e) {
+        e.preventDefault();
+        $('.r-collapsing__panel[data-tab ="' + $(this).children().data('tab') + '"]').show('slow').css({
+            'display': 'flex',
+            'z-index': '1000'
+        });
+    });
+    $('.icon-minus, .c-title').click(function (e) {
+        e.preventDefault();
+        $(".r-collapsing__panel").hide('slow').css('z-index', '0');
+    });
+});
+
+
+
+
