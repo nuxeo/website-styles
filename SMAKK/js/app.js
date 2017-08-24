989c611c02e4399dad05466c67bea83148b917c7
namurray <namurray85@gmail.com>
2017-08-24 13:56:37 -0400

    Page updates

--- SMAKK/js/app.js
+++ SMAKK/js/app.js
@@ -8,10 +8,11 @@ $(document).foundation();
 $(function() {
     $(".c-collapsing").click(function (e) {
         e.preventDefault();
-        $('.r-collapsing__panel[data-tab ="' + $(this).children().data('tab') + '"]').show('slow').css({
+        $('.r-collapsing__panel[data-tab ="' + $(this).data('tab') + '"]').show('slow').css({
             'display': 'flex',
             'z-index': '1000'
         });
+        console.log($(this).children())
     });
     $('.icon-minus, .c-title').click(function (e) {
         e.preventDefault();
