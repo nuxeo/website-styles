c47d6fad52909df34ee1def4cd796e64b58bd376
namurray <namurray85@gmail.com>
2017-08-27 11:57:46 -0400

    Commit JS to include tabs and sticky

--- SMAKK/js/custom_scripts.js
+++ SMAKK/js/custom_scripts.js
@@ -1,5 +1,5 @@
 //COLLAPSING CARDS
-$(function() {
+$(function () {
     $(".c-collapsing").click(function (e) {
         e.preventDefault();
         $('.r-collapsing__panel[data-tab ="' + $(this).data('tab') + '"]').show('slow').css({
@@ -16,14 +16,12 @@ $(function() {
 
 //INTEGRATIONS MENU
 
-$(function(){
-    $('.integrations__item:last-child').after("<li class=\"integrations-arrow\">➡</li>");
-        $(".integrations__item:nth-of-type(n+8)").hide();
-        $('.integrations-arrow').click(function(e) {
-            $(".integrations__item").toggle("slide");
-            $(this).css({
-                'transform': 'rotate(180deg)'
-            });
-            return false;
-        });
+$(function () {
+    $('.integrations.with-arrow > .integrations__item:last-child').after("<li class=\"integrations-arrow\">➡</li>");
+    $(".integrations__item:nth-of-type(n+8)").hide();
+    $('.integrations-arrow').click(function (e) {
+        $(this).siblings().toggle("slide");
+        $(this).toggleClass('flip');
+        return false;
+    });
 });
