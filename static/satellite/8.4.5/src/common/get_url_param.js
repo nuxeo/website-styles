11d2d8662cb83b21b3cf7113391d2127473ec569
Romain Bonhomme <romain.bonhomme89@gmail.com>
2018-10-25 17:02:44 -0400

    fixed menu top

--- static/satellite/8.4.5/src/common/get_url_param.js
+++
@@ -1,1 +0,0 @@
-module.exports=function(sParam){var sPageURL=decodeURIComponent(window.location.search.substring(1)),sURLVariables=sPageURL.split("&"),sParameterName,i;for(i=0;i<sURLVariables.length;i++){sParameterName=sURLVariables[i].split("=");if(sParameterName[0]===sParam){return void 0===sParameterName[1]?!0:sParameterName[1]}}};
