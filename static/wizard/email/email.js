8627304e0fb40ff6f65de4e67c936c1371f7a73f
Andrew Goodricke <github@andrewgoodricke.com>
2018-12-07 19:10:59 +0000

    Email Signature updated

--- static/wizard/email/email.js
+++ static/wizard/email/email.js
@@ -2,32 +2,32 @@
 /* global Handlebars Clipboard*/
 'use strict';
 
-$(document).ready(function () {
+$.getJSON("config.json", function(config) {
+  $(document).ready(function () {
     var templates_config = [
-        {
-            key     : 'standard',
-            template: 'email_signature_standard.hbs'
-        },
-        {
-            key     : 'plain',
-            template: 'email_signature_plain.hbs'
-        }
+      {
+        key     : 'standard',
+        template: 'email_signature_standard.hbs'
+      },
+      {
+        key     : 'plain',
+        template: 'email_signature_plain.hbs'
+      }
     ];
     var templates = {};
     var templates_loaded = false;
 
     /* Normal loops don't create a new scope */
     $(templates_config).each(function () {
-        var key = this.key;
-        var template = this.template;
-        $.get(template, function (data) {
-            templates[key] = Handlebars.compile(data);
-            if (Object.keys(templates).length === templates_config.length) {
-                templates_loaded = true;
-            }
-        });
+      var key = this.key;
+      var template = this.template;
+      $.get(template, function (data) {
+        templates[key] = Handlebars.compile(data);
+        if (Object.keys(templates).length === templates_config.length) {
+          templates_loaded = true;
+        }
+      });
     });
-
 
     var $input_div = $('#inputs');
     var $code = $('#code');
@@ -39,39 +39,46 @@ $(document).ready(function () {
     var $result = $('.result');
 
     $input_div.on('keyup change', 'input', function () {
-        var data = {};
-        var required_satisfied = true;
-        var compiled;
-        var signature_style;
+      var data = {};
+      var required_satisfied = true;
+      var compiled;
+      var signature_style;
+      var flare;
 
-        $inputs.each(function () {
-            /* eslint no-invalid-this: 0 */
-            var $this = $(this);
-            if ($this.prop('required') && !this.value) {
-                required_satisfied = false;
-            }
-            data[this.id] = this.value || '';
-        });
+      $inputs.each(function () {
+        /* eslint no-invalid-this: 0 */
+        var $this = $(this);
+        if ($this.prop('required') && !this.value) {
+          required_satisfied = false;
+        }
+        data[this.id] = this.value || '';
+      });
+      flare = $input_div.find('input[name="flare"]:checked').val();
+      data.flare = flare;
+      if (data.flare) {
+        data.flare_info = config.flare[data.flare];
+      }
 
-        if (required_satisfied && templates_loaded) {
-            signature_style = $input_div.find('input[name="signature_style"]:checked').val();
-            compiled = templates[signature_style](data);
-            $code.text(compiled);
-            $html.html(compiled);
-            $signature_link.attr('href', 'source.html?' + $inputs.serialize());
-            $result.show();
-        }
-        else {
-            $result.hide();
-        }
+      if (required_satisfied && templates_loaded) {
+        signature_style = $input_div.find('input[name="signature_style"]:checked').val();
+        compiled = templates[signature_style](data);
+        $code.text(compiled);
+        $html.html(compiled);
+        $signature_link.attr('href', 'source.html?' + $inputs.serialize());
+        $result.show();
+      }
+      else {
+        $result.hide();
+      }
     });
 
     $type_selector.on('click', '.is-email-type', function () {
-        var $this = $(this);
-        var id = $this.attr('id');
-        $type_content.hide().filter('#' + id.slice(3)).show();
+      var $this = $(this);
+      var id = $this.attr('id');
+      $type_content.hide().filter('#' + id.slice(3)).show();
     });
 
     /* eslint no-new:0 */
     new Clipboard('.copy-html');
+  });
 });
--- static/wizard/email/source.js
+++ static/wizard/email/source.js
@@ -2,54 +2,60 @@
 /* global Handlebars */
 'use strict';
 
-$(document).ready(function () {
+$.getJSON("config.json", function(config) {
+  $(document).ready(function () {
     var getUrlParameter = function getUrlParameter (sParam) {
-        var sPageURL = decodeURIComponent(window.location.search.substring(1));
-        var sURLVariables = sPageURL.split('&');
-        var sParameterName;
-        var i;
+      var sPageURL = decodeURIComponent(window.location.search.substring(1));
+      var sURLVariables = sPageURL.split('&');
+      var sParameterName;
+      var i;
 
-        for (i = 0; i < sURLVariables.length; i++) {
-            sParameterName = sURLVariables[i].split('=');
+      for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
 
-            if (sParameterName[0] === sParam) {
-                return sParameterName[1] === void 0 ? true : sParameterName[1];
-            }
+        if (sParameterName[0] === sParam) {
+          return sParameterName[1] === void 0 ? true : sParameterName[1];
         }
-        return void 0;
+      }
+      return void 0;
     };
 
     var param_names = [
-        'name',
-        'position',
-        'phone_label',
-        'phone',
-        'linkedin',
-        'twitter',
-        'facebook',
-        'gplus',
-        'github'
+      'name',
+      'position',
+      'phone_label',
+      'phone',
+      'linkedin',
+      'twitter',
+      'facebook',
+      'gplus',
+      'github',
+      'flare'
     ];
 
     var param_data = {};
     var param_value;
     var replace_body = false;
     for (var i = 0; i < param_names.length; i++) {
-        var param = param_names[i];
-        param_value = getUrlParameter(param);
-        // console.log('param', param, param_value);
-        if (param_value) {
-            replace_body = true;
-        }
-        param_data[param] = param_value;
+      var param = param_names[i];
+      param_value = getUrlParameter(param);
+      // console.log('param', param, param_value);
+      if (param_value) {
+        replace_body = true;
+      }
+      param_data[param] = param_value;
+    }
+    if (param_data.flare) {
+      param_data.flare_info = config.flare[param_data.flare];
     }
 
     var signature_style = getUrlParameter('signature_style');
     if (replace_body && signature_style) {
-        $.get('email_signature_' + signature_style + '.hbs', function (source) {
-            var template = Handlebars.compile(source);
+      $.get('email_signature_' + signature_style + '.hbs', function (source) {
+        var template = Handlebars.compile(source);
 
-            $('body').html(template(param_data));
-        });
+        $('body').html(template(param_data));
+      });
     }
+  });
 });
